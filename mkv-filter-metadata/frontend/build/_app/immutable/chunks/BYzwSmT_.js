360e1265a698131158982d5922727bbce8e251af
CuatesHD <dojeda2@outlook.com>
2026-06-05 14:42:40 -0700

    Untrack build directory and save latest frontend changes

--- mkv-filter-metadata/frontend/build/_app/immutable/chunks/BYzwSmT_.js
+++
@@ -1,1 +0,0 @@
-import{b2 as o,U as t,a$ as c}from"./B6IXPnOU.js";function f(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function u(e){t===null&&f(),o(()=>{const n=c(e);if(typeof n=="function")return n})}export{u as o};
