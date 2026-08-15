e9c88495e1b5ca32f99903031970f00890c9367b
CuatesHD <dojeda2@outlook.com>
2026-08-15 09:35:06 -0700

    Modified sha-256 values for sidecars in download-sidecars.mjs file

--- mkv-subtitle-converter/scripts/download-sidecars.mjs
+++ mkv-subtitle-converter/scripts/download-sidecars.mjs
@@ -7,19 +7,19 @@ import { finished } from 'node:stream/promises';
 // Replace this with the URL of the GitHub Release you created in Phase 2
 // NOTE: The hash map below is tightly coupled to the binaries hosted at this URL.
 // If you update the URL or rebuild the binaries, you MUST regenerate and update the hashes below.
-const REPO_URL = "https://github.com/Cuates/rust/releases/download/v1.0.0-binaries";
+const REPO_URL = "https://github.com/Cuates/rust/releases/download/v2.0.0-binaries";
 const TARGET_DIR = path.resolve(process.cwd(), 'backend/sidecars');
 
-// Map of filename to expected SHA-256 hash. 
+// Map of filename to expected SHA-256 hash.
 // Replace null with actual hashes for production security.
 const sidecars = {
-    "ffmpeg-aarch64-apple-darwin": "9a08d61f9328e8164ba560ee7a79958e357307fcfeea6fe626b7d66cdc287028",
-    "ffmpeg-x86_64-apple-darwin": "3a0ea97adddecfbf87b865da3bcbb321edfce4bab18a98ae1ba4ba9f0bd1f93a",
-    "ffmpeg-x86_64-pc-windows-msvc.exe": "228d7a8556258de907fdb55f36850078ebc7680b84ec30d84ea02e99bec1d1eb",
+    "ffmpeg-aarch64-apple-darwin": "591260c945d0eef150e3bf82b0ef988bd36a9cecc18ff05d6679617159f0a95e",
+    "ffmpeg-x86_64-apple-darwin": "f6db556b9e00083dbb22fc28c2370e07f1373a5402a20bfd3b22dd33cc6eeb8f",
+    "ffmpeg-x86_64-pc-windows-msvc.exe": "227af0691433b703ffc5725e47f7d06eefc34b4a72e7870e73d30e2cda483ecf",
     "ffmpeg-x86_64-unknown-linux-gnu": "e7e7fb30477f717e6f55f9180a70386c62677ef8a4d4d1a5d948f4098aa3eb99",
-    "ffprobe-aarch64-apple-darwin": "aab17ac7379c1178aaf400c3ef36cdb67db0b75b1a23eeef2cb9f658be8844e6",
-    "ffprobe-x86_64-apple-darwin": "a976306bcb8c9c50b2ac4e91f5aac4e45395e1f9063c46aecf1e1213e41c631b",
-    "ffprobe-x86_64-pc-windows-msvc.exe": "0fde260f5abd35c9cafd96f594cc76365a780c1b73a90e35b6a3409ea1db1bf0",
+    "ffprobe-aarch64-apple-darwin": "e11c17e8200b3ee4c4c186d245e2b4053f01d56957336c1817fca0b997469106",
+    "ffprobe-x86_64-apple-darwin": "248c015ea397b31a342029d7693baf284d2396895726dad3b1ea17905d6f2f20",
+    "ffprobe-x86_64-pc-windows-msvc.exe": "901f0efe4793cbb0f017101e3427f816e8fbf9a407bd585f49df30f4325cfd88",
     "ffprobe-x86_64-unknown-linux-gnu": "4f231a1960d83e403d08f7971e271707bec278a9ae18e21b8b5b03186668450d"
 };
 
